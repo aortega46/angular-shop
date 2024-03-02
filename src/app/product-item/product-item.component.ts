363500274309c6092f055831d4c6f18fae1ecf27
Albert Ortega <trebla.og46@gmail.com>
2024-03-02 17:07:32 +0100

    addToCart for product components

--- src/app/product-item/product-item.component.ts
+++ src/app/product-item/product-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core'
+import { Component, EventEmitter, Input, Output } from '@angular/core'
 import { MatIconModule } from '@angular/material/icon'
 import { Product } from '../interfaces/product'
 
@@ -12,4 +12,10 @@ export class ProductItemComponent {
   // * At this point,signal inputs exists so it can be done with that
   // https://angular.io/guide/signal-inputs#why-should-we-use-signal-inputs-and-not-input
   @Input() product!: Product
+
+  @Output() onAddToCart = new EventEmitter<Product>()
+
+  addToCart() {
+    this.onAddToCart.emit(this.product)
+  }
 }
