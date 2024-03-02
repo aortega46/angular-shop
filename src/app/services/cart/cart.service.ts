bb61eca79ca9447d9f40d54f80ab63b2e674e26f
Albert Ortega <trebla.og46@gmail.com>
2024-03-02 17:05:42 +0100

    Cart service addToCart
    Interface CartProduct

---
+++ src/app/services/cart/cart.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core'
+import { Product } from '../../interfaces/product'
+import { BehaviorSubject } from 'rxjs'
+import { CartProduct } from '../../interfaces/cart-product'
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CartService {
+  cart = new BehaviorSubject<CartProduct[]>([])
+
+  addToCart(product: Product) {
+    const currentCart = this.cart.getValue()
+    const productInCartIndex = currentCart.findIndex(
+      item => item.id === product.id
+    )
+
+    if (productInCartIndex >= 0) {
+      const newCart = [
+        ...currentCart.slice(0, productInCartIndex),
+        {
+          ...currentCart[productInCartIndex],
+          quantity: currentCart[productInCartIndex].quantity + 1,
+        },
+        ...currentCart.slice(productInCartIndex + 1),
+      ]
+
+      return this.cart.next(newCart)
+    }
+
+    const newCart = [...currentCart, { ...product, quantity: 1 }]
+    return this.cart.next(newCart)
+  }
+}
