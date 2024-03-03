c0602e704b4f3b8ec3408fa700f09e4ac30e523d
Albert Ortega <trebla.og46@gmail.com>
2024-03-03 10:38:27 +0100

    testing product-item.component

--- src/app/product-item/product-item.component.spec.ts
+++ src/app/product-item/product-item.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing'
 
 import { ProductItemComponent } from './product-item.component'
 import { Product } from '../interfaces/product'
+import { CartService } from '../services/cart/cart.service'
+import { inject } from '@angular/core'
 
 const productMock: Product = {
   id: 1,
@@ -17,6 +19,7 @@ describe('ProductItemComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ProductItemComponent],
+      providers: [CartService],
     }).compileComponents()
 
     fixture = TestBed.createComponent(ProductItemComponent)
@@ -34,5 +37,23 @@ describe('ProductItemComponent', () => {
   it('should match snapshot', () => {
     const compiled = fixture.nativeElement
     expect(compiled).toMatchSnapshot()
+  })
+
+  it('should call service when #addToCart', () => {
+    const service = TestBed.inject(CartService)
+    const spyAddToCart = jest.spyOn(service, 'addToCart')
+
+    component.addToCart()
+
+    expect(spyAddToCart).toHaveBeenCalledWith(component.product)
+  })
+
+  it('should call service when #removeFromCart', () => {
+    const service = TestBed.inject(CartService)
+    const spyRemoveFromCart = jest.spyOn(service, 'removeFromCart')
+
+    component.removeFromCart()
+
+    expect(spyRemoveFromCart).toHaveBeenCalledWith({ id: component.product.id })
   })
 })
